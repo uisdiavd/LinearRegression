0ee3cbade4c6124f299c35d9e45dfd0ae5fafb31
uisdiavd <abcd.davidsiu@gmail.com>
2024-05-07 03:21:45 -0700

    Reorganized existing code into classes and functions

--- tests/test_dataprep.py
+++ tests/test_dataprep.py
@@ -16,19 +16,22 @@ class TestYouTubeChannelDataManager(unittest.TestCase):
     def test_process_yt_channel_data(self):
         """ Test if it prepares views data by country with 'top_200_youtubers.csv' file """
         CSV = 'top_200_youtubers.csv'
-        data_manager = YouTubeChannelDataManager(CSV)
+        data_manager = YouTubeChannelDataManager()
         data_manager.process_yt_channel_data(CSV)
 
-#class TestAverageViewsLinearRegressionDataPreparation(unittest.TestCase):
+#        def test_avgviews_data_filter(self):
+#           """It should create a table with only views data in it"""
+#           pass
+
+#       def test_avgviews_by_country(self):
+#           """It should combine the data from avgviewsdatafilter by country"""
+#           pass
+#class TestLinearRegressionDataPreparation(unittest.TestCase):
 #    """Test cases for linear regression data preparation"""
 #
-#    def test_avgviews_data_filter(self):
-#        """It should create a table with only views data in it"""
-#        pass
+
 #    
-#    def test_avgviews_by_country(self):
-#        """It should combine the data from avgviewsdatafilter by country"""
-#        pass
+
 #    
 #    def test_convert_to_dataframe(self):
 #        """It should return dataframes for both training and target data"""
@@ -41,3 +44,5 @@ class TestYouTubeChannelDataManager(unittest.TestCase):
 #    def test_zero_data_handling(self):
 #        """It should skip linear regression model fitting when training dataframe is empty"""
 #        pass
+
+#class TestLinearRegression:
