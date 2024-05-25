a149b1bb29d988c59ddeee3a6d81dd497a650c30
uisdiavd <abcd.davidsiu@gmail.com>
2024-05-25 03:00:20 -0700

    Initial tests

--- fntest.py
+++ fntest.py
@@ -4,21 +4,40 @@ import logging
 import warnings
 from service.models import YouTubeChannelDataManager, LinearRegressionDataPreparation, FitData
 from service import app
+import pandas as pd
 
 file = 'top_200_youtubers.csv'
-row = 2
 
-print(f'row: {row}')
+#""" Test if only one entry exists for each country when preparing the 'top_200_youtubers.csv' file """
+#dfr = YouTubeChannelDataManager().process_yt_channel_data(file)
+#            
+## Testing that only one entry exists for each country
+#start = 1
+#for country in dfr['Country']:
+#    print(f"Testing country {country}")
+#    length = len(dfr['Country'])
+#    app.logger.info("Looking for duplicates of country %s", country)
+#    if country in dfr['Country'].values[start:length]:
+#        print(f"Multiple of country entry found for {country}")
+#        break
+#    else:
+#        start += 1
+    
+df = pd.read_csv(file)
 
-data = LinearRegressionDataPreparation().extract_target_data_for_row(file, row)
-print(f'data: \n {data}')
+# Set columns of target data
+cols = ['Avg. 1 Day', 'Avg. 3 Day', 'Avg. 7 Day', 'Avg. 14 Day', 'Avg. 30 day', 'Avg. 60 day']
+counter = 1
 
-#y = LinearRegressionDataPreparation().clean_target_data(file, row)
-#print(f'y: {y}')
+# Set groupby_filter
+groupby_filter = 'Country'
 
-trainings, testreturn = LinearRegressionDataPreparation().clean_training_data(file, row)
-clean = LinearRegressionDataPreparation().convert_to_dataframe(trainings)
-print(f'clean: {clean}')
+# Clean country formatting
+for country in df['Country']:
+    df['Country'][counter] = str(country).strip()
+    print(counter, country)
+    counter += 1
 
-printthis = FitData().linear_regression_model_fit(file, row)
-print(printthis)
+views = df.groupby(groupby_filter)[cols].mean().fillna(0)
+dfr = pd.DataFrame(views).reset_index()
+print(dfr)
